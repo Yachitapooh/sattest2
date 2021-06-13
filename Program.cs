ef468f40a3b5674d8a401d3f1a65c3647dfc05d8
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-06-13 17:17:15 +0700

    k3

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace sattestfinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ข้อ3
+            string number;
+            do
+            {
+                number = Console.ReadLine();
+            }
+            while (number == "End");
+
+            string text = Console.ReadLine();
+            int min, max;
+            int sum;
+            if (text == "FindMax")
+            {
+                Console.WriteLine("{0}", Math.max(text));
+            }
+            else if (text == "FindMin")
+            {
+
+            }
+            else if (text == "FindMead")
+            {
+                Console.WriteLine("{0}", sum / number );
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
